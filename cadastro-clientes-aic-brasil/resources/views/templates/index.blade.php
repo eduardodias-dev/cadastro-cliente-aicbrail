fbcaf32df28fd6a7bc73fddfea7cd985a8ed4d2a
Eduardo Dias <eduardo.dias092@gmail.com>
2024-07-19 20:03:41 -0300

    link visitas

--- cadastro-clientes-aic-brasil/resources/views/templates/index.blade.php
+++ cadastro-clientes-aic-brasil/resources/views/templates/index.blade.php
@@ -147,6 +147,14 @@
                     </p>
                 </a>
             </li>
+            <li class="nav-item ">
+              <a href="{{route('visitas')}}" class="nav-link {{ request()->is('visitas') ? 'active' : '' }}">
+                  <i class="nav-icon far fa-users-alt"></i>
+                  <p>
+                      Visitas
+                  </p>
+              </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
